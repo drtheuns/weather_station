84c3f13a464a997efb2dc70737869a8e56b34621
Randall Theuns <randall.theuns@gmail.com>
2018-01-11 11:03:17 +0100

    Add initial structure for event handling

--- weather_station/weather_station.ino
+++ weather_station/weather_station.ino
@@ -2,6 +2,11 @@
 #include <ESP8266HTTPClient.h>
 #include "ArduinoJson.h"
 
+// change these to match the actual pins on the arduino
+const int motion_pin = 2;
+const int button_pin = 3;
+const int audio_pin  = 8;
+
 const char* ssid = "ssid";
 const char* password = "password";
 
@@ -63,7 +68,27 @@ JsonObject& getWeatherInformation()
     return root;
 }
 
+// should the event type be sent to this function (e.g. motion vs button event)?
+void handleEvent()
+{
+    // Handle both motion and button pressed events here.
+    const JsonObject& weatherJson = getWeatherInformation();
+}
+
 void loop()
 {
-    delay(5000);
+    const int motion_value = digitalRead(motion_pin);
+    const int button_value = digitalRead(button_pin);
+
+    // if both handle events are the same, they could be in the same `if`.
+    if (motion_value == HIGH) {
+        // motion detected
+    }
+
+    if (button_value == HIGH) {
+        // button pressed
+    }
+
+    // should delay be longer when motion is detected to avoid multiple checks?
+    delay(100);
 }
